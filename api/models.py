1d57e44d83e7fde96d98488ee380b2a61f0bfaaf
SultokTheF <sultok.003@gmail.com>
2024-10-30 18:48:28 +0500

    feat: models ids removed

--- api/models.py
+++ api/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Vehicle(models.Model):
-    vehicle_id = models.CharField(max_length=50, primary_key=True)
     type = models.CharField(max_length=50)
     capacity = models.IntegerField()
     operator = models.CharField(max_length=100)
@@ -9,7 +8,6 @@ class Vehicle(models.Model):
     status = models.CharField(max_length=50)  # Active, Inactive, Maintenance, etc.
 
 class Route(models.Model):
-    route_id = models.CharField(max_length=50, primary_key=True)
     name = models.CharField(max_length=100)
     start_point = models.CharField(max_length=100)
     end_point = models.CharField(max_length=100)
@@ -17,13 +15,11 @@ class Route(models.Model):
     average_travel_time = models.DurationField()
 
 class Stop(models.Model):
-    stop_id = models.CharField(max_length=50, primary_key=True)
     name = models.CharField(max_length=100)
     location_coordinates = models.CharField(max_length=100)
     zone = models.CharField(max_length=50)
 
 class Schedule(models.Model):
-    schedule_id = models.AutoField(primary_key=True)
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
     route = models.ForeignKey(Route, on_delete=models.CASCADE)
     departure_time = models.DateTimeField()
@@ -32,7 +28,6 @@ class Schedule(models.Model):
     status = models.CharField(max_length=50)
 
 class RealTimeLocation(models.Model):
-    record_id = models.AutoField(primary_key=True)
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
     route = models.ForeignKey(Route, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -41,7 +36,6 @@ class RealTimeLocation(models.Model):
     speed = models.FloatField()  # Speed in km/h
 
 class Prediction(models.Model):
-    prediction_id = models.AutoField(primary_key=True)
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
     stop = models.ForeignKey(Stop, on_delete=models.CASCADE)
     predicted_arrival_time = models.DateTimeField()
@@ -49,7 +43,6 @@ class Prediction(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
 class HistoricalData(models.Model):
-    history_id = models.AutoField(primary_key=True)
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
     route = models.ForeignKey(Route, on_delete=models.CASCADE)
     stop = models.ForeignKey(Stop, on_delete=models.CASCADE)
